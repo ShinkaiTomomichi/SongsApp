c0e85ea867683a39107abec252c5fe75df77e1a2
ShinkaiTomomichi <yahharogim@gmail.com>
2021-09-04 13:09:04 +0900

    単体テストのサンプルを追加

---
+++ SongsApp/SongsAppTests/SampleTests.swift
@@ -0,0 +1,25 @@
+//
+//  SampleTests.swift
+//  SongsAppTests
+//
+//  Created by shinkaitomomichi on 2021/09/04.
+//
+
+import XCTest
+
+class SampleTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        XCTAssertTrue(true)
+        XCTAssertFalse(false)
+        XCTAssertEqual("0", "0")
+    }
+}
